b60f06227bcc395c1096bb1b5171c7231c3df7ab
Ullrich Herdler <ullrich.herdler@gmail.com>
2020-01-26 14:36:36 +0100

    textBox1 receives Data from DB.

--- HH_Csharp-ReadDB001/Form1.cs
+++ HH_Csharp-ReadDB001/Form1.cs
@@ -28,5 +28,24 @@ namespace HH_Csharp_ReadDB001
             this.odbcConnection = new OdbcConnection(odbcConnectionString);
             this.odbcConnection.Open();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string readQuery = "SELECT * from simple_table";
+            OdbcCommand odbcCommand = new OdbcCommand(readQuery);
+            odbcCommand.Connection = this.odbcConnection;
+            OdbcDataReader odbcDataReader = odbcCommand.ExecuteReader();
+
+          
+            if (odbcDataReader.HasRows)
+            {
+                while (odbcDataReader.Read())
+                {
+                    // dataGridView1.Rows.Add(odbcDataReader[0].ToString(), odbcDataReader[1].ToString());
+                    textBox1.Text += odbcDataReader[1].ToString() + Environment.NewLine;
+                }
+            }          
+            odbcDataReader.Close();
+        }
     }
 }
